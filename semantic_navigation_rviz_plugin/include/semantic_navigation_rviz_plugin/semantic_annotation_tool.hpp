26f9faf6703adec3f44e62bc7565b8189bb72253
Alberto Tudela <ajtudela@gmail.com>
2024-04-23 08:43:33 +0200

    Update license in files
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

--- semantic_navigation_rviz_plugin/include/semantic_navigation_rviz_plugin/semantic_annotation_tool.hpp
+++ semantic_navigation_rviz_plugin/include/semantic_navigation_rviz_plugin/semantic_annotation_tool.hpp
@@ -1,13 +1,16 @@
-/*
- * SEMANTIC ANNOTATION RVIZ TOOL
- *
- * Copyright (c) 2021-2023 Alberto José Tudela Roldán <ajtudela@gmail.com>
- * 
- * This file is part of semantic_navigation.
- * 
- * All rights reserved.
- *
- */
+// Copyright (c) 2020 Alberto J. Tudela Roldán
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef SEMANTIC_NAVIGATION__SEMANTIC_ANNOTATION_TOOL_HPP_
 #define SEMANTIC_NAVIGATION__SEMANTIC_ANNOTATION_TOOL_HPP_
